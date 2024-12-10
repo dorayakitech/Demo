7220351802ca0946db356a9fd2a8c804bd1d54c4
tang-xiaolong <1097145586@qq.com>
2024-12-11 01:39:15 +0800

    Add move component

---
+++ Assets/Scripts/Battle/Move/IMoveAble.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public interface IMoveAble
+{
+    void MoveToTarget(Transform target);
+    void MoveToPosition(Vector3 position);
+}
---
+++ Assets/Scripts/Battle/Move/LineMove.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class LineMove : MonoBehaviour, IMoveAble
+{
+    public float MoveSpeed = 5f;
+    public Transform Target;
+    public Vector3? TargetPosition;
+    public void MoveToTarget(Transform target)
+    {
+        Target = target;
+    }
+
+    public void MoveToPosition(Vector3 position)
+    {
+        TargetPosition = position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Target.position, MoveSpeed * Time.fixedDeltaTime);
+        }
+        else if (TargetPosition != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, TargetPosition.Value, MoveSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
