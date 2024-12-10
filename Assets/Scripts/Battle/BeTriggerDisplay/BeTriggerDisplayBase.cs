3e13469a1a3ec9f3756a970223f08b628ba2bc69
tang-xiaolong <1097145586@qq.com>
2024-12-11 01:42:50 +0800

    Add BeTriggerDisplay components

---
+++ Assets/Scripts/Battle/BeTriggerDisplay/BeTriggerDisplayBase.cs
@@ -0,0 +1,28 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+/// <summary>
+/// 被碰到后的表现脚本
+/// 发事件、播放特效、播放打击音效、子弹弹坑贴花等
+/// </summary>
+public abstract class BeTriggerDisplayBase : SerializedMonoBehaviour
+{
+    [Required]
+    public ObjectFilter Filter;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Filter == null)
+            return;
+        bool isMatch = Filter.HasMatch(other.gameObject);
+
+        if (isMatch)
+        {
+            //触碰点
+            var point = other.ClosestPointOnBounds(transform.position);
+            DisplayHandle(point);
+        }
+    }
+
+    protected virtual void DisplayHandle(Vector3 touchPoint)
+    {
+    }
+}
---
+++ Assets/Scripts/Battle/BeTriggerDisplay/SendEventWhenBeTrigger.cs
@@ -0,0 +1,13 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class SendEventWhenBeTrigger : BeTriggerDisplayBase
+{
+    [Required]
+    public SOEvent Event;
+    protected override void DisplayHandle(Vector3 touchPoint)
+    {
+        if(Event != null)
+            Event.Notify();
+    }
+}
